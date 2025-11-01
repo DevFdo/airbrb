7d4422f4802b5f17d06c6facd6bef01facfdf0eb
Yunqing <z5544201@ad.unsw.edu.au>
2025-11-01 22:29:40 +1100

    Login page created. Able to navigate to Login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,80 @@
+import React,{useState} from 'react';
+
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <Container component="main" maxWidth="sm">
+            <Box
+                sx={{
+                    boxShadow: 3,
+                    borderRadius: 2,
+                    px: 4,
+                    py: 6,
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                <Typography component="h1" variant="h5">
+                    Log In
+                </Typography>
+                <Box
+                    component="form"
+                    //onSubmit={handleSubmit}
+                    noValidate
+                    sx={{mt:1}}>
+                    <TextField
+                        margin="normal"
+                        fullWidth
+                        required
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoFocus
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Log In
+                    </Button>
+                    <Grid container>
+                        <Grid item>
+                            <Link href="/register"
+                                  variant="body2"
+                            >
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    );
+};
+
+export default Login;
