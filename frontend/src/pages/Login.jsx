f2892878636dd3023b34d140f8d3f5b2f7d1d059
Yunqing <z5544201@ad.unsw.edu.au>
2025-11-02 19:12:59 +1100

    Lint style fixed for Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import {useState} from 'react';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
@@ -14,94 +14,87 @@ import { API_BASE_URL } from '../config';
 
 const Login = () => {
     
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(API_BASE_URL+'/user/auth/login', {
+      email: email, password: password,},
+    {headers: {'Content-Type': 'application/json'}}
+    )
+    if (response.status === 200) {
+      alert("Successfully Logged In");
+      localStorage.setItem('token', response.data.token);
+      navigate('/');
+    }
+    else{
+      alert("Can not Log in:"+response.data.error);
+    }
+  };
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const response = await axios.post(API_BASE_URL+'/user/auth/login', {
-                email: email,
-                password: password,
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        )
-        if (response.status === 200) {
-            alert("Successfully Logged In");
-            localStorage.setItem('token', response.data.token);
-            navigate('/');
-        }
-        else{
-            alert("Can not Log in:"+response.data.error);
-        }
-    };
-
-    return (
-        <Container component="main" maxWidth="sm">
-            <Box
-                sx={{
-                    boxShadow: 3,
-                    borderRadius: 2,
-                    px: 4,
-                    py: 6,
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}>
-                <Typography component="h1" variant="h5">
-                    Log In
-                </Typography>
-                <Box
-                    component="form"
-                    onSubmit={handleSubmit}
-                    noValidate
-                    sx={{mt:1}}>
-                    <TextField
-                        margin="normal"
-                        fullWidth
-                        required
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoFocus
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Log In
-                    </Button>
-                    <Grid container>
-                        <Grid item>
-                            <Link href="/register"
-                                  variant="body2"
-                            >
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container component="main" maxWidth="sm">
+      <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Typography component="h1" variant="h5">
+          Log In
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{mt:1}}>
+          <TextField
+            margin="normal"
+            fullWidth
+            required
+            id="email"
+            label="Email Address"
+            name="email"
+            autoFocus
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Log In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/register"
+                variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
