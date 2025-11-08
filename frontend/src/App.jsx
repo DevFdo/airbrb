6de527e9d90000cfb7526496f59306bdd402a00e
Devin Fernando <devinfernando@student.unsw.edu.au>
2025-11-08 20:07:42 +1100

    Added the routes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route,Navigate } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Login from './pages/Login';
 import Register from "./pages/Register.jsx";
+// Feature set 2
+import HostedListings from './pages/HostedListings.jsx';
+import CreateListing from './pages/CreateListing.jsx';
+import EditListing from './pages/EditListing.jsx';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      {/* Feature set 2 routes */}
+      <Route path="/host/listings" element={<HostedListings />} />
+      <Route path="/host/listings/new" element={<CreateListing />} />
+      <Route path="/host/listings/:id/edit" element={<EditListing />} />
     </Routes>
   );
 }
