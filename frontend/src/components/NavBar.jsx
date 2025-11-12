0765991382312d4f39b4f97f5f0162ee60b4c1b9
Devin Fernando <devinfernando@student.unsw.edu.au>
2025-11-12 15:19:56 +1100

    Removed the use of alert and instead used the MUI snack bar component to show messages

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import {Button, Menu, MenuItem} from "@mui/material";
+import {Button, Menu, MenuItem, Snackbar, Alert} from "@mui/material";
 
 import {API_BASE_URL} from "../config.js";
 
@@ -18,6 +18,12 @@ export default function NavBar() {
 
   const [auth,setAuth] = useState(false);
   const [anchorEl, setAnchorEl] = React.useState(null);
+
+    // snackbar state
+    const [snackOpen, setSnackOpen] = useState(false);
+    const [snackMsg, setSnackMsg] = useState('');
+    // 'success' | 'error' | 'warning' | 'info'
+    const [snackSeverity, setSnackSeverity] = useState('success'); 
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -32,6 +38,10 @@ export default function NavBar() {
     setAnchorEl(null);
   };
 
+  const handleSnackClose = () => {
+    setSnackOpen(false);
+  };
+
   const handleLogout = async (e) => {
     e.preventDefault();
     console.log(localStorage.getItem('token'));
@@ -44,89 +54,110 @@ export default function NavBar() {
       }
     )
     if (response.status === 200) {
-      alert("Successfully Logged Out!");
+      setSnackMsg('Successfully logged out');
+      setSnackSeverity('success');
+      setSnackOpen(true);
       localStorage.removeItem('token');
       localStorage.removeItem('email');
       setAuth(false);
-      navigate('/');
+      setTimeout(() => {
+        navigate('/');
+      }, 1000);
+     
     }
     else{
-      alert("Can not Log out:"+response.data.error);
+      setSnackMsg('Could not log out: ' + (err.response?.data?.error || err.message));
+      setSnackSeverity('error');
+      setSnackOpen(true);
     }
   };
 
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={handleMenu}
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        {auth && (
-          <div>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              slotProps={{
-                list: {
-                  'aria-labelledby': 'basic-button',
-                },
-              }}
-            >
-              <MenuItem
-                onClick={() => {
-                  handleClose();
-                  navigate('/host/listings');
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenu}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {auth && (
+            <div>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                slotProps={{
+                  list: {
+                    'aria-labelledby': 'basic-button',
+                  },
                 }}
               >
-                My Hosted Listings
-              </MenuItem>
-              <MenuItem
-                onClick={() => {
-                  handleClose();
-                  navigate('/');
-                }}
-              >
-                All Listings
-              </MenuItem>
-            </Menu>
-          </div>
-        )}
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    navigate('/host/listings');
+                  }}
+                >
+                  My Hosted Listings
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    navigate('/');
+                  }}
+                >
+                  All Listings
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
 
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        AIRBRB
-        </Typography>
-        {auth && (
-          <div>
-            <Button
-              color="inherit"
-              onClick={handleLogout}
-              sx={{variant: 'contained'}}>
-              Log out
-            </Button>
-          </div>
-        )}
-        {!auth && (
-          <div>
-            <Button
-              color="inherit"
-              sx={{ variant: 'contained'}}
-              component={Link}
-              to="/login">
-              Get Started
-            </Button>
-          </div>
-        )}
-      </Toolbar>
-    </AppBar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          AIRBRB
+          </Typography>
+          {auth && (
+            <div>
+              <Button
+                color="inherit"
+                onClick={handleLogout}
+                sx={{variant: 'contained'}}>
+                Log out
+              </Button>
+            </div>
+          )}
+          {!auth && (
+            <div>
+              <Button
+                color="inherit"
+                sx={{ variant: 'contained'}}
+                component={Link}
+                to="/login">
+                Get Started
+              </Button>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+
+      {/* Snackbar for messages */}
+      <Snackbar
+        open={snackOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert onClose={handleSnackClose} severity={snackSeverity} sx={{ width: '100%' }}>
+          {snackMsg}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
